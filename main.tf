3b254a6b65b24488035df8c7e67357b44da6466c
kapil7042 <kapilsharma704220@gmail.com>
2024-06-20 10:16:08 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@
 
 
 resource "aws_instance" "Demo" {
-  ami           = var.ami
-  instance_type = var.instance_type
+  ami           = "$(var.ami)"
+  instance_type = "$(var.instance_type)"
   tags = {
     Name = "demo"
     
